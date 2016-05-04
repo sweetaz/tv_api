1e0bdb27b05a5d0b27f3883b6cb88339a08bbc07
Aaron Glover <sweetaz@gmail.com>
2016-05-04 23:45:25 +1200

    fixed typo in server launch line

--- server.sh
+++ server.sh
@@ -3,11 +3,11 @@ mix deps.get --only prod
 MIX_ENV=prod mix compile
 
 # Compile assets
-brunch build --production
+#brunch build --production
 MIX_ENV=prod mix phoenix.digest
 
 # Custom tasks (like DB migrations)
 MIX_ENV=prod mix ecto.migrate
 
 # Finally run the server
-PORT=4001 MIX_ENV=prod elixir --detatched -S mix do compile, phoenix.server
+PORT=4001 MIX_ENV=prod elixir --detached -S mix do compile, phoenix.server
